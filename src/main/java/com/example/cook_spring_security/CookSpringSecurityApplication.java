b62df55a34a5b557017307a174090b5541accf89
it <email@e.com>
2022-10-03 09:13:00 +0300

    init spring-oauth2-open-id-connect

---
+++ src/main/java/com/example/cook_spring_security/CookSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.cook_spring_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cook_spring_security/CookSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cook_spring_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
