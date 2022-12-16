56a91e96d628dad8cad15d1aa094c59513df4269
it <email@e.com>
2022-12-16 15:05:38 +0300

    init

---
+++ docker-keycloak-postgres-deploy/src/test/java/com/example/dockerkeycloakpostgresdeploy/DockerKeycloakPostgresDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerkeycloakpostgresdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerKeycloakPostgresDeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
